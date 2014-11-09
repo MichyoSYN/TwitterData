125de126d5527231685e89204bb5f3978172e3cb
Michyo <michyosyn@gmail.com>
2014-11-09 17:10:14 +0800

    add event lists but not working now.

--- DataAnalyse/views.py
+++ DataAnalyse/views.py
@@ -1,15 +1,29 @@
-from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.views import generic
 from DataAnalyse.models import Twitter
+from django.shortcuts import render_to_response
 
 # Create your views here.
 
-def index(request):
-    latest_twits_list = Twitter.objects.order_by('-pub_date')[:5]
-    context = {'latest_twits_list': latest_twits_list}
-    return render(request, 'DataAnalyse/index.html', context)
+def test(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
 
-def detail(request, id):
-    twit = get_object_or_404(Twitter, pk = id)
-    return render(request, 'DataAnalyse/detail.html', {'twit': twit})
+class IndexView(generic.ListView):
+    template_name = 'DataAnalyse/index.html'
+    context_object_name = 'latest_twits_list'
 
+    def get_queryset(self):
+        # Return the last five published questions.
+        return Twitter.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Twitter
+    template_name = 'DataAnalyse/detail.html'
+    context_object_name =  'twit'
